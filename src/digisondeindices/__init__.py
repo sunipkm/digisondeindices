d25763d3e2c3ef8d418606748a75e291c2c7b65f
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2023-02-03 17:26:52 -0500

    Published to pypi

--- src/digisondeindices/__init__.py
+++ src/digisondeindices/__init__.py
@@ -1,20 +1,5 @@
 from .base import get_indices
 
-def cli():
-    """
-    simple demo of retrieving DIDBase indices by date
-    """
-    from argparse import ArgumentParser
+__version__ = '2.0.2'
 
-    p = ArgumentParser()
-    p.add_argument("date", help="time of observation yyyy-mm-ddTHH:MM:ss")
-    p.add_argument("-s", "--station", help='Observation station code e.g. AH223', type=str)
-    a = p.parse_args()
-
-    inds = get_indices(a.date, p.station)
-
-    print(inds)
-
-
-if __name__ == "__main__":
-    cli()
+__all__ = ['get_indices', '__version__']
--- src/digisondeindices/base.py
+++ src/digisondeindices/base.py
@@ -56,3 +56,22 @@ def todatetime(time: str | date | datetime | np.datetime64, tzaware: bool = True
     dates = np.atleast_1d(d).ravel()
 
     return dates
+
+def cli():
+    """
+    simple demo of retrieving DIDBase indices by date
+    """
+    from argparse import ArgumentParser
+
+    p = ArgumentParser()
+    p.add_argument("date", help="time of observation yyyy-mm-ddTHH:MM:ss")
+    p.add_argument("-s", "--station", help='Observation station code e.g. AH223', type=str)
+    a = p.parse_args()
+
+    inds = get_indices(a.date, p.station)
+
+    print(inds)
+
+
+if __name__ == "__main__":
+    cli()
